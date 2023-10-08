0ee03d425f7ed12ce41c484393fb8c4cbf80f6fa
huix-chen <Chenhuixin2015@hotmail.com>
2023-10-08 13:18:24 +1100

    Modify analysis_test.R: [describe your changes here]

--- analysis_test.R
+++ analysis_test.R
@@ -1,7 +1,11 @@
 # Example R script
 data <- c(1, 2, 3, 4, 5)
+regressor=c(6,7,8,9,0)
 mean_value <- mean(data)
 print(mean_value)
 
 varaince=var(data)
 variance
+
+##Fit a linear model
+lm(data~regressor)
