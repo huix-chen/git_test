0609b66fc5b1644a210bed214ed797e24e251ccb
huix-chen <Chenhuixin2015@hotmail.com>
2023-10-10 09:53:49 +1100

    Add function analysis.R

--- analysis.R
+++ analysis.R
@@ -2,3 +2,13 @@
 data <- c(1, 2, 3, 4, 5)
 mean_value <- mean(data)
 print(mean_value)
+
+
+logistic = function(x, y) {
+  # For example, you can calculate the logistic function
+  result = 1 / (1 + exp(-(x + y)))
+  # Return the result
+  return(result)
+}
+
+
